fe93b1efe912e8840716359ad9ed13cdf73f588a
Jeremy Sun <herecomesthesun1223@gmail.com>
2021-02-17 00:34:53 -0500

    email service

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { MDBMask, MDBRow, MDBCol, MDBBtn, MDBView, MDBContainer, MDBAnimation, MDBBox } from "mdbreact";
+import { MDBMask, MDBRow, MDBCol, MDBBtn, MDBView, MDBContainer, MDBAnimation, MDBBox, MDBIcon } from "mdbreact";
 import Nav from './Nav';
 import '../styles/Home.scss'
 import welcome from '../assets/placeholder-frame.png';
@@ -9,14 +9,54 @@ import {
     Route,
     Link
 } from "react-router-dom";
+import emailjs from 'emailjs-com'
+
+const serviceid = 'service_gmail';
+const userid = 'user_sgHKs82f6tKP9sjht8mbs';
+const template_welcome = 'template_welcome';
 
 export class Home extends Component {
+    
+    /* Get list of subscribers from database */
+    getSubscribers() {
+        //LOGIC GOES HERE
+        return [{name_to: 'Jeremy', email_to: 'sunnykid1223@gmail.com'}, {name_to: 'Dennis', email_to: 'dennisbae7@gmail.com'}];
+    }
+    
     //Image frame tool: https://browserframe.com/
+
+    /*
+        sendWelcomeEmail sends a single email
+        template must be a valid template id. 
+        templateParams must be an object of dynamic variables
+
+        Subscriber object:
+        {
+            name_to: 'Recepient name',
+            email_to: 'recepient@gmail.com'
+        }
+    */
+    sendEmail (e, template, templateParams) {
+        e.preventDefault();
+
+        emailjs.send(serviceid, template, templateParams, userid)
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+    }
+
+    /* For mailing to all subscribers */
+    sendEmailList (e, template) {
+        var subscribers = this.getSubscribers(); //list of subscriber objects
+        subscribers.forEach((subscriber) => {this.sendEmail(e, template, subscriber)});
+        console.log('Emails sent')
+    }
+
     render() {
         return (
             <React.Fragment>
-                
-               
                 
                 {/*Wrapper*/}
                 <MDBContainer fluid className="d-flex flex-column justify-content-center align-items-center px-0 backdrop">
@@ -53,7 +93,8 @@ export class Home extends Component {
                             </MDBRow>
                             <MDBRow className="my-2">
                                 <MDBCol>
-                                <MDBBtn color="deep-orange" size="lg">SUBSCRIBE</MDBBtn>
+                                    {/*sendEmailList is for testing purposes. Replace this.sendEmailList with this.sendEmail, passing a user object {name_to: , email_to: } */}
+                                <MDBBtn onClick={(e, template) => this.sendEmailList(e, template_welcome)}color="deep-orange" size="lg"><b>SUBSCRIBE <MDBIcon icon="bell" /></b></MDBBtn>
                                 </MDBCol>
                             </MDBRow>
                         </MDBContainer>
